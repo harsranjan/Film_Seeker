fef08628509dc5f4a30f201dae61b349443bb251
Harsh Ranjan <96332194+harsranjan@users.noreply.github.com>
2024-12-24 02:08:22 +0530

    Add files via upload

---
+++ FrontEnd/data.js
@@ -0,0 +1,22 @@
+let movies = [
+  {
+    name: "Netflix",
+    des: "Netflix is an American subscription video on-demand over-the-top streaming service. The service primarily distributes original and acquired films and television shows from various genres, and it is available internationally in multiple languages.",
+    image: "img/netflix.png",
+  },
+  {
+    name: "Disney+ Hotstar",
+    des: "Disney+ Hotstar is an Indian subscription video-on-demand over-the-top streaming service owned by Reliance Industries and Disney India, featuring domestic Indian film, television and sports content for India itself and its worldwide diaspora. It also includes imported content from and serves Southeast Asia as well.",
+    image: "img/disney-hotstar.jpg"
+  },
+  {
+    name: "Amazon Prime Video",
+    des: "Amazon Prime Video, or simply Prime Video, is an American subscription video on-demand over-the-top streaming and rental service of Amazon offered both as a stand-alone service and as part of Amazon's Prime subscription.",
+    image: "img/prime.webp"
+  },
+  {
+    name: "YouTube",
+    des: "YouTube (YT) is an American online video sharing platform owned by Google. Accessible worldwide,[note 1] YouTube was launched on February 14, 2005, by Steve Chen, Chad Hurley, and Jawed Karim. Headquartered in San Bruno, California, United States, it is the second-most visited website in the world, after Google Search.",
+    image: "img/youtube.jpg"
+  },
+];
